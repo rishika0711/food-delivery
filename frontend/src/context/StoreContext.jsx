fb6bd72edb039e85a969558968edab19d6f46b60
Rishika Singh <55060904+rishika0711@users.noreply.github.com>
2025-03-10 12:14:22 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -11,7 +11,7 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
-    const url = "http://localhost:4000";
+    const url = "https://food-delivery-ee4m.onrender.com";
     
     const addToCart = async(itemId) => {
         if (!cartItems[itemId]) {
